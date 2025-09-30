7379808bf530b3922766ab8c456b2ce8371a4761
ArunLohani <arun.lohani@argusoft.in>
2025-09-30 18:37:10 +0530

    Change Migration from yaml to xml format

--- src/main/java/com/project/societyManagement/entity/User.java
+++ src/main/java/com/project/societyManagement/entity/User.java
@@ -2,21 +2,26 @@ package com.project.societyManagement.entity;
 
 import com.project.societyManagement.common.AuditableEntity;
 import com.project.societyManagement.entity.type.Role;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
+
 
 @Data
 @Entity
 @Table(name = "users")
 public class User extends AuditableEntity {
 
+
     private String name;
 
+    @Column(nullable = false,unique = true)
     private String email;
     private String password;
+    @Column(length = 10 , name = "phone_number")
     private Integer phoneNumber;
+
+    @Enumerated(EnumType.STRING)
     private Role role;
 
 
